93698cea3ac3fca066fedca8f4a0b902ddc40e67
Alireza Khodadadi <alirezakh0199@gmail.com>
2024-06-10 09:05:44 +0330

    add login

--- graphql/resolves.js
+++ graphql/resolves.js
@@ -2,8 +2,8 @@ import { v4 as uuidv4 } from 'uuid';
 import mongoose from 'mongoose';
 
 // !! Error Message Function
-function ErrorMessage(message) {
-  return new GraphQLError('the error message', {
+function ErrorMessage(message = 'the error message') {
+  return new GraphQLError(message, {
     extensions: {
       code: 'SOMETHING_BAD_HAPPENED',
       http: {
@@ -17,11 +17,62 @@ function ErrorMessage(message) {
   });
 }
 
+// Schema
+const { Schema } = mongoose;
+const UserSchema = new Schema(
+  {
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  {
+    collection: 'User',
+    versionKey: false,
+    toJSON: {
+      virtuals: true,
+      transform: (doc, ret) => {
+        // delete ret._id;
+        delete ret.id;
+        // delete ret.password;
+      },
+    },
+    toObject: {
+      virtuals: true,
+      transform: (doc, ret) => {
+        // delete ret._id;
+        delete ret.id;
+        // delete ret.password;
+      },
+    },
+  }
+);
+
+const UserModel = mongoose.model('User', UserSchema);
 // !! Resolvers
 const resolvers = {
   Query: {
     getSource: async () => {
       return { url: 'http://localhost:8000/graphql' };
+    },
+  },
+  Mutation: {
+    login: async (_root, props, context) => {
+      const { username, password } = props;
+      // 1 ) Check Validation Props
+      if (!username || !password) {
+        ErrorMessage('Please Enter Username,Password');
+      }
+      // 2 ) DataBase Search
+      const user = await UserModel.findOne({ username, password });
+      if (!user) {
+        ErrorMessage('username or password wrong!');
+      }
+      return {
+        message: 'success',
+        status: 200,
+        data: {
+          user,
+        },
+      };
     },
   },
 };
